10bfebdd11fc2ddc337702ae54b96ba0d3d3f86a
Adrian R. <arodriguez20042008@gmail.com>
2025-11-17 01:05:59 +0100

    funcionalidad del valor de los ases

--- DevTalles_JS/BlackJack/assets/js/game.js
+++ DevTalles_JS/BlackJack/assets/js/game.js
@@ -3,6 +3,14 @@ let deck = [];
 const suits = ["H", "D", "C", "S"]; //Palos de la baraja
 const letterCards = ["A", "J", "Q", "K"]; //Cartas que no contienen numeros
 
+let playerPoints = 0,
+  dealerPoints = 0,
+  ascesCount = 0;
+
+const btnTake = document.querySelector("#btn-take");
+const HTMLplayerPoints = document.querySelector("#player-points");
+const HTMLdealerPoints = document.querySelector("#dealer-points");
+
 //Crear la baraja ya mezclada
 const createDeck = () => {
   // Crear las cartas del 2 al 10
@@ -39,10 +47,26 @@ const cardValue = (card) => {
   //Los strings en js pueden ser tratados como arrays. Substring extre el valor de la carta desde la posicion establecida, en este caso 0, hasta la ultima posicion de la carta -1. Que seria siempre el palo
   const value = card.substring(0, card.length - 1);
 
-  return (points = isNaN(value) ? (value === "A" ? 11 : 10) : parseInt(value));
+  return isNaN(value) ? (value === "A" ? 11 : 10) : parseInt(value);
 };
 
 console.log(createDeck());
-const carta = takeCard();
-console.log(carta);
-console.log(cardValue(carta));
+
+btnTake.addEventListener("click", () => {
+  const card = takeCard();
+  let thisValue = cardValue(card);
+
+  //Aumentar contador ases
+  if (thisValue === 11) ascesCount++;
+  playerPoints += thisValue;
+
+  //Siempre que el jugador supere 21 y tenga algun as en la mano, este necesitara que valga 1 y no 11, para ello se le restara 10 a los puntos del jugador y quita el as del contador
+  while (playerPoints > 21 && ascesCount > 0) {
+    playerPoints -= 10;
+    ascesCount--;
+  }
+
+  HTMLplayerPoints.innerHTML = playerPoints;
+
+  console.log(card, playerPoints);
+});
