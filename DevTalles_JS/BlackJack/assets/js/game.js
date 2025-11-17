8ea012da5dc484b3130f67984deb87c8013e157d
adrirf7 <arodriguez20042008@gmail.com>
2025-11-17 14:33:52 +0100

    Añadir cartas a la mano

--- DevTalles_JS/BlackJack/assets/js/game.js
+++ DevTalles_JS/BlackJack/assets/js/game.js
@@ -1,17 +1,21 @@
-let deck = [];
+let deck = []; //Baraja
 
 const suits = ["H", "D", "C", "S"]; //Palos de la baraja
 const letterCards = ["A", "J", "Q", "K"]; //Cartas que no contienen numeros
 
+//*Variables del juego
 let playerPoints = 0,
   dealerPoints = 0,
   ascesCount = 0;
 
+//* Elementos del HTML
 const btnTake = document.querySelector("#btn-take");
 const HTMLplayerPoints = document.querySelector("#player-points");
 const HTMLdealerPoints = document.querySelector("#dealer-points");
 
-//Crear la baraja ya mezclada
+const HTMLplayerCards = document.querySelector("#player-cards");
+
+//*Crear la baraja ya mezclada
 const createDeck = () => {
   // Crear las cartas del 2 al 10
   for (let i = 2; i <= 10; i++) {
@@ -31,7 +35,7 @@ const createDeck = () => {
   return deck;
 };
 
-//Tomar una carta
+//*Tomar una carta
 const takeCard = () => {
   //Verifica si la baraja esta vacia
   if (deck.length === 0) {
@@ -42,16 +46,18 @@ const takeCard = () => {
   return deck.pop();
 };
 
-//Extraer el valor de la carta
+//*Extraer el valor de la carta
 const cardValue = (card) => {
   //Los strings en js pueden ser tratados como arrays. Substring extre el valor de la carta desde la posicion establecida, en este caso 0, hasta la ultima posicion de la carta -1. Que seria siempre el palo
   const value = card.substring(0, card.length - 1);
 
+  //Si no es un numero solo hay dos opciones As o Figura. El as incialemnte vale 11, y las figuras 10. Si es numero devuelve el valor de la carta
   return isNaN(value) ? (value === "A" ? 11 : 10) : parseInt(value);
 };
 
-console.log(createDeck());
+//? Eventos De los botones
 
+//*Tomar una carta
 btnTake.addEventListener("click", () => {
   const card = takeCard();
   let thisValue = cardValue(card);
@@ -67,6 +73,8 @@ btnTake.addEventListener("click", () => {
   }
 
   HTMLplayerPoints.innerHTML = playerPoints;
-
+  HTMLplayerCards.innerHTML += `<img class="game-card" src="assets/cartas/${card}.png" />`;
   console.log(card, playerPoints);
 });
+
+console.log(createDeck());
